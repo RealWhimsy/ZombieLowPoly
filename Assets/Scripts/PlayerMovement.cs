1caf842ea753e9591f0c35c8d18fb89a4cfadb03
Fabian Schiller <schiller.fabian25@gmail.com>
2021-01-01 17:53:09 +0100

    [refactor] Added main menu.. again

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -222,17 +222,11 @@ public class PlayerMovement : MonoBehaviour
 
     private void MouseMovement()
     {
-        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
-        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
-        float rayLength;
-
-        if (groundPlane.Raycast(cameraRay, out rayLength))
-        {
-            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
-            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
-
-            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
-        }
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.y - transform.position.y)); 
+        Vector3 lookDirection = new Vector3(worldPos.x, transform.position.y, worldPos.z);
+        transform.LookAt(lookDirection);
+        Debug.DrawLine(Camera.main.transform.position, lookDirection, Color.red);
+        
         if (Input.GetMouseButton(0) && !weaponLogic.reloading)
         {
             if (weaponLogic.bulletsRemaining > 0 && (Time.time - shotTime > getEquipedGun().shotCooldown) || melee)
