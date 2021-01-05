7fea409a03adbd306896d5ac901443621a46df60
Fabian Schiller <schiller.fabian25@gmail.com>
2021-01-05 18:12:54 +0100

    [feature] Added InteractiblesManager and Selfdestroy scripts
    - added interactibles and selfdestroy script
    - added scripts to prefabs / zombies

---
+++ Assets/Scripts/InteractiblesManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiblesManager : MonoBehaviour
+{
+
+    private System.Random rand = new System.Random();
+    private int selector = -1;
+    private int propability = -1;
+    private GameObject interactible;
+
+    public void SpawnInteractible()
+    {
+        propability = rand.Next(0, 3);
+        if(propability == 2)
+        {
+            Instantiate(SelectInteractible(), transform.position, transform.rotation);
+        }
+        
+    }
+
+    private GameObject SelectInteractible()
+    {
+        interactible = new GameObject();
+        selector = rand.Next(0, 3);
+        switch (selector)
+        {
+            case 0:
+                interactible = Resources.Load("Prefabs/box_ammo") as GameObject;
+                break;
+            case 1:
+                interactible = Resources.Load("Prefabs/box_med") as GameObject;
+                break;
+            case 2:
+                interactible = Resources.Load("Prefabs/box_supply") as GameObject;
+                break;
+        }
+        return interactible;
+    }
+
+    
+}
