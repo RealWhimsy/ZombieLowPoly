37d3f4d63bb2fe7e67c8c456c69e295c1f101c5c
RealWhimsy <lukas.jack@web.de>
2021-01-09 15:12:37 +0100

    Prevent double hit on melee attack
    Prevents hitting the same target multiple times with one melee attack by saving a list of already hit targets. This list resets once a new melee attack is started.

--- Assets/Scripts/MeleeAttack.cs
+++ Assets/Scripts/MeleeAttack.cs
@@ -1,9 +1,14 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class MeleeAttack : MonoBehaviour, IDamageDealer
 {
     private GameObject player;
     private PlayerManager playerManager;
+    
+    // List that contains all colliders already hit by melee attack. Deals no damage if already hit,
+    // to prevent double hits on the same target
+    private List<Collider> alreadyHitUnits;
 
     void Start()
     {
@@ -11,13 +16,21 @@ public class MeleeAttack : MonoBehaviour, IDamageDealer
         playerManager = player.GetComponent<PlayerManager>();
     }
 
+    private void OnEnable()
+    {
+        // Reset the list of already hit targets on new melee attack
+        alreadyHitUnits = new List<Collider>();
+    }
+
     void OnTriggerEnter(Collider collision)
     {
         Debug.Log(collision);
         IDamageable damageable = collision.gameObject.GetComponent(typeof(IDamageable)) as IDamageable;
-        if (damageable != null)
+
+        if (damageable != null && !alreadyHitUnits.Contains(collision))
         {
             Debug.Log("DAMAGE" + collision);
+            alreadyHitUnits.Add(collision);
             damageable.TakeDamage(this);
         }
         
