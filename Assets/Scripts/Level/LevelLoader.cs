40060eb691741a200006ef78f929243b70556195
Fabian Schiller <schiller.fabian25@gmail.com>
2021-01-20 14:56:28 +0100

    [feature] Added level loader
    - added levelloader
    - added constants for scenes
    - added canvas to dontdestroyonload
    - removed obsolete manager in desert, city and pirateBay
    - changed existing canvas to debugcanvas

---
+++ Assets/Scripts/Level/LevelLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventManager.StartListening(Const.Events.LevelCompleted, LoadNextLevel);
+    }
+
+
+    private void LoadNextLevel()
+    {
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case Const.SceneNames.Forest:
+                StartCoroutine(LoadLevel(Const.SceneNames.Desert));
+                break;
+            case Const.SceneNames.Desert:
+                StartCoroutine(LoadLevel(Const.SceneNames.PirateBay));
+                break;
+            case Const.SceneNames.PirateBay:
+                StartCoroutine(LoadLevel(Const.SceneNames.City));
+                break;
+        }
+    }
+
+    private IEnumerator LoadLevel(string sceneName)
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    
+}
