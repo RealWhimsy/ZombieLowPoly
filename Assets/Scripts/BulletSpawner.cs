33c837e0984f67967a3951376491aff388368817
orembold <62057626+orembold@users.noreply.github.com>
2021-01-24 16:23:32 +0100

    [fixed] Shotgun bullets now spread randomly
    -Shotguns now instantiate six bullets at the same time

--- Assets/Scripts/BulletSpawner.cs
+++ Assets/Scripts/BulletSpawner.cs
@@ -8,11 +8,15 @@ public class BulletSpawner : MonoBehaviour
     private PlayerManager playerManager;
     private Weapon weapon;
     private GameObject grenade;
+    private Transform playerBullet;
+    private Bullet bulletScript;
+    private float randSpread;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         playerManager = player.GetComponent<PlayerManager>();
         weapon = playerManager.GetActiveWeapon();
+
     }
 
     private void OnEnable()
@@ -31,11 +35,26 @@ public class BulletSpawner : MonoBehaviour
 
     private void Shoot()
     {
-        var randSpread = Random.Range(-weapon.BulletSpread, weapon.BulletSpread);
-        var spread = Quaternion.Euler(0, 0 + randSpread, 0);
-        Transform playerBullet = Instantiate(weapon.Bullet.transform, transform.position, transform.rotation * spread);
-        Bullet bulletScript = playerBullet.GetComponent<Bullet>();
-        bulletScript.setDamage(weapon.Damage);
+        if(weapon.WeaponType == WeaponType.Shotgun) {
+            Shotgun();
+        }
+        else{
+            randSpread = Random.Range(-weapon.BulletSpread, weapon.BulletSpread);
+            var spread = Quaternion.Euler(0, 0 + randSpread, 0);
+            Transform playerBullet = Instantiate(weapon.Bullet.transform, transform.position, transform.rotation * spread);
+            Bullet bulletScript = playerBullet.GetComponent<Bullet>();
+            bulletScript.setDamage(weapon.Damage);
+        }
+    }
+
+    private void Shotgun(){
+
+        for(int i=0; i< Const.Shotgun.ShotgunSplinters; i++){
+        randSpread = Random.Range(-weapon.BulletSpread, weapon.BulletSpread);
+        playerBullet = Instantiate(weapon.Bullet.transform, transform.position, transform.rotation * Quaternion.Euler(0, randSpread, 0));
+        bulletScript = playerBullet.GetComponent<Bullet>();
+        bulletScript.setDamage(weapon.Damage / Const.Shotgun.ShotgunSplinters);
+        }
     }
 
     private IEnumerator ThrowGrenade()
