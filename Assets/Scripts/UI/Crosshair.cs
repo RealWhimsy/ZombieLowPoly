64f0f4aafed862572e3c6de7237a70d0a8c3cd22
Fabian Schiller <schiller.fabian25@gmail.com>
2021-01-24 11:42:01 +0100

    [feature] Added crosshair  (as laser) to player
    - added lineRenderer as laser to player
    - adjusted bulletSpawner coordinates

---
+++ Assets/Scripts/UI/Crosshair.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cursor.visible = false;
+        lineRenderer = gameObject.GetComponent<LineRenderer>();
+        lineRenderer.startWidth = 0.05f;
+        lineRenderer.endWidth = 0.025f;
+        lineRenderer.useWorldSpace = false;
+		lineRenderer.positionCount = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		var pos = new Vector3(0, transform.position.y, -20);
+		var endPos = new Vector3(pos.x, pos.y, pos.z + 400);
+        lineRenderer.SetPosition(0, pos);
+		lineRenderer.SetPosition(1, endPos);
+    }
+}
