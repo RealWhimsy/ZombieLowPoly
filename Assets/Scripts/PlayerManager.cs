18037be8cb4dd1a7355afbe3a5b9a8583d96f702
Fabian Schiller <schiller.fabian25@gmail.com>
2021-02-06 17:59:26 +0100

    [hotfix] Fixed issue #129

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -167,6 +167,7 @@ public class PlayerManager : MonoBehaviour, IDamageable
     {
         yield return new WaitForSeconds(Const.Player.RespawnTime);
         SetSpawnStats();
+        ResetSupplies();
 		EventManager.TriggerEvent(Const.Events.PlayerRespawned);
     }
 
