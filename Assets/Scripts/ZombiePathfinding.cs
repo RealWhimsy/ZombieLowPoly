bbfccb5b38540c21f9f7f0ff6438f318221b335e
RealWhimsy <lukas.jack@web.de>
2021-02-24 01:27:08 +0100

    fixed nullpointers in tutorial

--- Assets/Scripts/ZombiePathfinding.cs
+++ Assets/Scripts/ZombiePathfinding.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class ZombiePathfinding : MonoBehaviour
 {
@@ -35,6 +36,10 @@ public class ZombiePathfinding : MonoBehaviour
 
     private void SetCurrentDifficultyStats()
     {
+        if (SceneManager.GetActiveScene().name.Equals(Const.SceneNames.Tutorial))
+        {
+            return;
+        }
         agent.speed = Difficulty.CurrentDifficulty.ZombieSpeed;
     }
 
